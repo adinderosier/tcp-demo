89825b964f4b6b548a4e72c927b4b195205f99b2
Ethan Pressley <ethanpressley321@gmail.com>
2021-09-26 19:55:05 -0700

    Ship class prototype is complete

--- game/ship.c
+++ game/ship.c
@@ -1,9 +1,29 @@
+#include <stdlib.h>
+#include <assert.h>
+#include "ship.h"
+
 struct Ship {
 	int size;
 	int orientation;
-	int *loc;
 };
 
-struct Ship* create_ship() {
-	
+struct Ship* create_ship(int len, int orient) {
+	Ship *ship = calloc(sizeof(struct Ship), 1);
+	assert(ship != NULL);
+
+	ship->size = len;
+	ship->orientation = orient;
+	return ship;
+}
+
+void free_ship(struct Ship *ship) {
+	free(ship);
+}
+
+int get_size(struct Ship *ship) {
+	return ship->size;
+}
+
+int get_orientation(struct Ship *ship) {
+	return ship->orientation;
 }
--- game/ship.h
+++ game/ship.h
@@ -1,18 +1,15 @@
 #ifndef SHIP_H
 #define SHIP_H
 
-#define VERT 0
-#define HORIZ 1
-
 // forward declaration of struct
 struct Ship {};
 
 // struct 'constructor'
-struct Ship* create_ship(int len, int orient, int *locs);
+struct Ship* create_ship(int len, int orient);
+void free_ship(struct Ship *ship);
 
 // getters
 int get_size(struct Ship *ship);
 int get_orientation(struct Ship *ship);
-int* get_locations(struct Ship *ship);
  
 #endif // SHIP_H
